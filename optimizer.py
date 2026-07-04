5819dd32446d9694d3729c9b6011109397b540e6
Qurayz <33249423+Qurayz@users.noreply.github.com>
2026-07-04 14:04:04 +0300

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,23 +1,16 @@
 from pulp import *
 
 def optimize_lineup(df, budget=70, num_players=6):
-    """Select best lineup under budget"""
     if len(df) < num_players:
         return df.head(num_players)
     
     prob = LpProblem("Fantasy_Lineup", LpMaximize)
     select = LpVariable.dicts("select", df.index, cat="Binary")
     
-    # Maximize expected points
     prob += lpSum(select[i] * df.loc[i, 'xfp'] for i in df.index)
-    
-    # Budget constraint
     prob += lpSum(select[i] * df.loc[i, 'price'] for i in df.index) <= budget
-    
-    # Exactly 6 players
     prob += lpSum(select[i] for i in df.index) == num_players
     
-    # Max 3 per team
     for team in df['team'].unique():
         team_idx = df[df['team'] == team].index
         prob += lpSum(select[i] for i in team_idx) <= 3
